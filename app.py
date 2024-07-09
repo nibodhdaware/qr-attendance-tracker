82069c344640661a6fecd0dbed2e72cc729109ff
Nibodh Daware <55696034+nibodhdaware@users.noreply.github.com>
2024-07-09 16:34:34 +0530

    add download button

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def get_updated_students():
 def download_csv():
     directory = "."
     filename = "attendance.csv"
-    return send_from_directory(directory, filename)
+    return send_from_directory(directory, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=10000)
